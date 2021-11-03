c26955b931154c3ec1809fb565088028d6a7c7d8
Peter Sadej <31295774+petersadej@users.noreply.github.com>
2021-11-03 11:09:23 -0400

    Aiq-989, Aiq-990: Tag checking (#20)
    * Move changes to new branch
    
    * update PAT
    
    * fix yaml
    
    * install reqs
    
    * use different reqs file
    
    * add pytest install
    
    * Rearrange jobs in order of dependencies; Clean up dependency lists
    
    * add missing req
    
    * fix yaml
    
    * try different path
    
    * inspect local dir
    
    * add framework to python path
    
    * test
    
    * only run against changed pytest files
    
    * add test files
    
    * Allow for __init__.py files
    
    allow for init files in pytest directories
    
    fix allow init.py
    
    Make regex more specific
    
    Move init.py check to correct spot
    
    change syntax to extended regex
    
    update filename to pass check
    
    test new regex
    
    Delete test_FunctionalTemplate_bp453_.py
    
    Allow init.py to pass testcase name check
    
    fix logic order
    
    Update ci_workflow.yml
    
    Update ci_workflow.yml
    
    * Delete __init__.py
    
    * Combine tag checking jobs
    
    * remove old dependencies
    
    * update get_test_info to allow multiple input files
    
    * Add prep environment step to clean up output of inventory file
    
    * move pythonpath export to same step as tool run
    
    * fix import in test files
    
    * revert earlier changes
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * pytest tag checking
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * avoid using subshells
    
    * Add report to email and artifacts
    
    * Update ci_workflow.yml
    
    * fix variable refs
    
    * comment
    
    * Update ci_workflow.yml
    
    * run robot tool
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * add check for no changed files
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * create robot test files
    
    * Update ci_workflow.yml
    
    * robot test
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * Update ci_workflow.yml
    
    * add debug
    
    * Update ci_workflow.yml
    
    * put test files in correct folder
    
    * Update ci_workflow.yml
    
    * remove duplicate code; fix robot file extension check
    
    * update robot filename regex for new reqs
    
    * Remove oopsy
    
    * Update ci_workflow.yml
    
    * run robot parser for multiple input files
    
    * Add robot tag parsing; improve tag check output
    
    * pass along output
    
    * Update tag output display; remove test files
    
    * Update messages for consistency; Stop emailing misc txt files

--- Tests/Robot/get_test_info.py
+++ Tests/Robot/get_test_info.py
@@ -1,10 +1,32 @@
 from ExtremeAutomation.Utilities.Framework.test_case_inventory import RobotTestData
-import sys
+import argparse
+import json
+import os
+
+parser = argparse.ArgumentParser(description='Get info for Robot files.')
+parser.add_argument('files',
+                    type=str,
+                    nargs='+',
+                    help='a file to process')
+args = parser.parse_args()
 
 def main(path):
     TS = RobotTestData(path)
-    TS.print_suite(TS.testsObj)
+    output = TS.print_suite(TS.testsObj)
     print(f"qTest Tag Count: {TS.qTestTagCount}\nqTest Tags: {TS.qTestTags}")
 
+    return output
+
 if __name__ == '__main__':
-    main(sys.argv[1])
+    # main(sys.argv[1])
+    output_dict = {}
+    for file in args.files:
+        result = main(file)
+        # merge the result dict into the output dict
+        output_dict = {**output_dict, **result}
+        # output_dict[file] = result
+
+    # Output CICD info
+    print(json.dumps(output_dict))
+    with open(f'{os.getcwd()}/robot_data.json', 'w') as outfile:
+        json.dump(output_dict, outfile)
