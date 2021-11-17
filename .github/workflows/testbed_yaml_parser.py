a4c9b0db3cdebf49dd79a4adf07434d593ac514b
Peter Sadej <31295774+petersadej@users.noreply.github.com>
2021-11-17 12:24:17 -0500

    AIQ-1082: Add testbed yaml file checks (#46)
    * Update .gitignore
    
    * Add checks for testbed yaml files
    
    Check 1: Validate that the committed files are .yaml files
    Check 2: Validate that the keys in the YAML files are all lowercase
    
    * add test files
    
    * Add missing env var
    
    * fix reading testbed files list
    
    * add repo checkout
    
    * Fix exception raising
    
    * Don't do a clean checkout to avoid wiping CI files
    
    * flip download/checkout order. Remove pyyaml install
    
    Pyyaml is preinstalled
    
    * improve error reporting
    
    * strip newline
    
    * temporarily remove testbed file check
    
    * temporarily remove email attachments
    
    * redownload artifacts if needed
    
    * Fix email break on early failures
    
    * Run testbed yaml from external file
    
    * fix path
    
    * strip newline
    
    * Create empty reports
    
    * Remove test files and vestigial code
    
    * handle the no files to check scenario

---
+++ .github/workflows/testbed_yaml_parser.py
@@ -0,0 +1,62 @@
+import argparse
+from collections import abc
+from sys import exit
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="File that contains the list of testbed yaml files", type=str)
+args = parser.parse_args()
+
+# Comment to test without the need to read in a list of files from a file
+try:
+    with open(args.file, "r") as f:
+        list_of_testbed_files = f.read().strip()
+except Exception as e:
+    raise Exception('The CI script encountered a problem.') from e
+
+list_of_testbed_files = list_of_testbed_files.split(",") if list_of_testbed_files else  []
+
+# Uncomment to test without the need to read in a list of files from a file
+# list_of_testbed_files = ["TestBeds\\RDU\\Dev\\rdu_x590_pod5_3node.yam", "TestBeds\\RDU\\Dev\\rdu_x690_stk_pod1_3node.yaml"]
+
+rc=0
+
+# yields every key from a nested dict
+def nested_dict_iter(nested):
+    for key, value in nested.items():
+        if isinstance(value, abc.Mapping):
+            yield from nested_dict_iter(value)
+        else:
+            yield key
+
+def find_bad_keys(file):
+    global rc
+    try:
+        with open(file, "r") as stream:
+            testbed_file = yaml.safe_load(stream)
+            # print(testbed_file)
+    except Exception as e:
+        print(f"[*] FAIL: {file} failed! Unable to load Testbed YAML file. Exception: {e}")
+        rc=1
+        return
+
+    yaml_reader = nested_dict_iter(testbed_file)
+
+    offending_keys = []
+    for key in yaml_reader:
+        if not key.islower():
+            offending_keys.append(key)
+
+    if offending_keys:
+        print(f"[*] FAIL: {file} failed! Uppercase keys found.")
+        print(f"[**] Offending keys: {offending_keys}")
+        rc=1
+    else:
+        print(f"[*] PASS: {file} passed!")
+
+if list_of_testbed_files:
+    for file in list_of_testbed_files:
+        find_bad_keys(file)
+        print()
+
+exit(rc)
