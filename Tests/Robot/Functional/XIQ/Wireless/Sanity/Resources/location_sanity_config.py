f85f14edd20c8e01fdb116f5cd487f37f8e21278
Eric Latour <39134465+elatour2000@users.noreply.github.com>
2022-10-05 13:33:39 -0400

    [Skip Checks: Tags] elatour aiq 2313 (#842)
    * updated the tests
    
    * updated test
    
    * updated test
    
    * updated test
    
    * updated tests
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated

--- Tests/Robot/Functional/XIQ/Wireless/Sanity/Resources/location_sanity_config.py
+++ Tests/Robot/Functional/XIQ/Wireless/Sanity/Resources/location_sanity_config.py
@@ -2,6 +2,6 @@ import random
 
 RANDOM_INT = random.randint(0, 999)
 
-POLICY_NAME   =       f"Location_POLICY_{RANDOM_INT}"
-SSID_NAME     =       f"Location_SSID_{RANDOM_INT}"
+POLICY_NAME   =       f"Location_Sanity_POLICY_{RANDOM_INT}"
+SSID_NAME     =       f"Location_Sanity_SSID_{RANDOM_INT}"
 
