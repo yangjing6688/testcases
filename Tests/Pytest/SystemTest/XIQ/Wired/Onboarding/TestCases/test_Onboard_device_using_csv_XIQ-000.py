9b31b3c95128cc4a4e02327ed7888c39059f29ac
jsubler93 <78049885+jsubler93@users.noreply.github.com>
2023-01-04 11:44:20 -0500

    [Skip Checks: Reserved Tags, Tags, Func, File] Updated the system test case with new runlist logic (#1105)
    * Updated the system test case with new runlist logic
    
    * Removed the code that set the server ip info

--- Tests/Pytest/SystemTest/XIQ/Wired/Onboarding/TestCases/test_Onboard_device_using_csv_XIQ-000.py
+++ Tests/Pytest/SystemTest/XIQ/Wired/Onboarding/TestCases/test_Onboard_device_using_csv_XIQ-000.py
@@ -7,7 +7,7 @@ from Tests.Pytest.SystemTest.XIQ.Wired.Resources.SuiteUdks import SuiteUdks
 @pytest.mark.p1
 @pytest.mark.development
 @pytest.mark.testbed_1_node
-class OnboardDeviceVSP4900UsingCsvTests:
+class OnboardDeviceUsingCsvTests:
     """ Make sure that the location which is provided in the yaml file for dut1 is created in XIQ before the run of these tests.
 
     Ran these test cases when the location of dut1 is 'auto_location_01, Santa Clara, building_02, floor_04'.
@@ -17,7 +17,7 @@ class OnboardDeviceVSP4900UsingCsvTests:
     suite_udks = SuiteUdks()
 
     @pytest.fixture(scope="class")
-    def csv_file_location_1(self, dut1):
+    def csv_file_without_location(self, dut1):
         try:
             csv_file_location = self.suite_udks.create_csv_file(work_dir=self.current_directory,
                                         dut_serial=dut1.serial,
@@ -27,7 +27,7 @@ class OnboardDeviceVSP4900UsingCsvTests:
             self.suite_udks.delete_csv(csv_file_location)
 
     @pytest.fixture(scope="class")
-    def csv_file_location_2(self, dut1):
+    def csv_file_with_location(self, dut1):
         try:
             csv_file_location = self.suite_udks.create_csv_file(work_dir=self.current_directory,
                                                     dut_serial=dut1.serial,
@@ -46,13 +46,7 @@ class OnboardDeviceVSP4900UsingCsvTests:
             if loaded_config.get("lab", "").upper() == "SALEM":
                 cli.downgrade_iqagent(dut1.cli_type, spawn_connection)
 
-            cli.configure_device_to_connect_to_cloud(
-                dut1.cli_type, loaded_config['sw_connection_host'],
-                spawn_connection, vr=dut1.mgmt_vr, retry_count=30
-            )
 
-            if dut1.cli_type.upper() == "EXOS":
-                cli.send(spawn_connection, "enable iqagent")
 
     @pytest.fixture
     def cleanup(self, xiq_library_at_class_level, dut1):
@@ -63,14 +57,15 @@ class OnboardDeviceVSP4900UsingCsvTests:
             xiq_library_at_class_level.xflowscommonDevices.delete_device(device_serial=dut1.serial)
 
     @pytest.mark.tccs_7651
-    def test_onboard_device_using_csv_without_location(self, test_data, xiq_library_at_class_level, dut1, logger, csv_file_location_1, cleanup):
+    def test_onboard_device_using_csv_without_location(self, test_data, xiq_library_at_class_level, dut1, logger, csv_file_without_location, cleanup):
         '''[Documentation]  Test_Objective: Verify a device can be onboared using a csv file'''
 
-        res = xiq_library_at_class_level.xflowscommonDevices.onboard_device_quick(
-            {**dut1, "csv_location": csv_file_location_1, "device_make": dut1.make})
+        res = xiq_library_at_class_level.xflowscommonDevices.quick_onboarding_cloud_csv(device_make=dut1.make,
+                                                         csv_location=csv_file_without_location)
+
 
         if res != 1:
-            pytest.fail(f'Could not onboard device {dut1.platform} with serial {dut1.serial}')
+            logger.fail(f'Could not onboard device {dut1.platform} with serial {dut1.serial}')
 
         logger.info(f'Device {dut1.platform} with serial {dut1.serial} has been onboarded')
 
@@ -81,11 +76,11 @@ class OnboardDeviceVSP4900UsingCsvTests:
         if managed_res == 1:
             logger.info('Status for device with serial number: {} is equal to managed'.format(dut1.serial))
         else:
-            pytest.fail('Status for serial {} not equal to managed: {}'.format(dut1.serial, managed_res))
+            logger.fail('Status for serial {} not equal to managed: {}'.format(dut1.serial, managed_res))
 
         res = xiq_library_at_class_level.xflowscommonDevices.get_device_status(device_serial=dut1.serial)
         if res != 'green':
-            pytest.fail('Status for serial {} not equal to Green: {}'.format(dut1.serial, res))
+            logger.fail('Status for serial {} not equal to Green: {}'.format(dut1.serial, res))
         else:
             logger.info('Status for device with serial number: {} is equal to Green'.format(dut1.serial))
 
@@ -93,21 +88,20 @@ class OnboardDeviceVSP4900UsingCsvTests:
 
         location_to_verify = "Assign Location"
         if device_location != location_to_verify:
-            pytest.fail('Current location {} did not match the expected location {} assigned to device {} having serial {}'.format(
+            logger.fail('Current location {} did not match the expected location {} assigned to device {} having serial {}'.format(
                     device_location, location_to_verify, dut1.platform, dut1.serial))
         else:
             logger.info('Current location {} matched the expected location {} assigned to device {} having serial {}'.format(
                 device_location, location_to_verify, dut1.platform, dut1.serial))
 
-    @pytest.mark.tccs_7651
-    def test_onboard_device_using_csv_with_location(self, test_data, xiq_library_at_class_level, dut1, logger, csv_file_location_1, cleanup):
+    @pytest.mark.tccs_7652
+    def test_onboard_device_using_csv_with_location(self, test_data, xiq_library_at_class_level, dut1, logger, csv_file_without_location, cleanup):
         '''[Documentation]  Test_Objective: Verify a device can be onboared using a csv file and selecting the location field'''
 
-        res = xiq_library_at_class_level.xflowscommonDevices.quick_onboarding_cloud_csv(
-            csv_location=csv_file_location_1, location=",".join(dut1.location.split(", ")[1:]), device_make=dut1.make)
+        res = xiq_library_at_class_level.xflowscommonDevices.quick_onboarding_cloud_csv(device_make=dut1.make, location=dut1.location, csv_location=csv_file_without_location)
 
         if res != 1:
-            pytest.fail(f'Could not onboard device {dut1.platform} with serial {dut1.serial}')
+            logger.fail(f'Could not onboard device {dut1.platform} with serial {dut1.serial}')
         else:
             logger.info(f'Device {dut1.platform} with serial {dut1.serial} has been onboarded')
 
@@ -118,36 +112,37 @@ class OnboardDeviceVSP4900UsingCsvTests:
         if managed_res == 1:
             logger.info('Status for device with serial number: {} is equal to managed'.format(dut1.serial))
         else:
-            pytest.fail('Status for serial {} not equal to managed: {}'.format(dut1.serial, managed_res))
+            logger.fail('Status for serial {} not equal to managed: {}'.format(dut1.serial, managed_res))
 
         res = xiq_library_at_class_level.xflowscommonDevices.get_device_status(device_serial=dut1.serial)
         if res != 'green':
-            pytest.fail('Status for serial {} not equal to Green: {}'.format(dut1.serial, res))
+            logger.fail('Status for serial {} not equal to Green: {}'.format(dut1.serial, res))
         else:
             logger.info('Status for device with serial number: {} is equal to Green'.format(dut1.serial))
 
-        location_to_verify = self.suite_udks.expected_location_in_gui(",".join(dut1.location.split(", ")[1:]))
+        location_to_verify = self.suite_udks.expected_location_in_gui(dut1.location)
 
         device_location = self.suite_udks.get_value_specific_column(xiq_library_at_class_level, dut1.serial, "LOCATION")
 
         if location_to_verify not in device_location:
-            pytest.fail('Current location {} did not match the expected location {} assigned to device {}'.format(
+            logger.fail('Current location {} did not match the expected location {} assigned to device {}'.format(
                 device_location, location_to_verify, dut1.serial))
         else:
             logger.info('Current location {} matched the expected location {} assigned to device {}'.format(
                 device_location, location_to_verify, dut1.serial))
 
-    @pytest.mark.tccs_7651
-    def test_onboard_device_using_csv_with_location_in_csv(self, test_data, xiq_library_at_class_level, dut1, logger, csv_file_location_2, cleanup):
+    @pytest.mark.tccs_7653
+    def test_onboard_device_using_csv_with_location_in_csv(self, test_data, xiq_library_at_class_level, dut1, logger, csv_file_with_location, cleanup):
         '''[Documentation]  Test_Objective: Verify a device can be onboared using a csv file with location in csv file
         Starting in 22R5 version of XIQ the location in the csv file will be ignore, updated test case to check that the
         location is equal to Assign Location'''
 
-        res = xiq_library_at_class_level.xflowscommonDevices.onboard_device_quick(
-            {**dut1, "csv_location": csv_file_location_2, "device_make": dut1.make})
+        res = xiq_library_at_class_level.xflowscommonDevices.quick_onboarding_cloud_csv(device_make=dut1.make,
+                                                                                        csv_location=csv_file_with_location)
+
 
         if res != 1:
-            pytest.fail(f'Could not onboard device {dut1.platform} with serial {dut1.serial}')
+            logger.fail(f'Could not onboard device {dut1.platform} with serial {dut1.serial}')
         else:
             logger.info(f'Device {dut1.platform} with serial {dut1.serial} has been onboarded')
 
@@ -158,11 +153,11 @@ class OnboardDeviceVSP4900UsingCsvTests:
         if managed_res == 1:
             logger.info('Status for device with serial number: {} is equal to managed'.format(dut1.serial))
         else:
-            pytest.fail('Status for serial {} not equal to managed: {}'.format(dut1.serial, managed_res))
+            logger.fail('Status for serial {} not equal to managed: {}'.format(dut1.serial, managed_res))
 
         res = xiq_library_at_class_level.xflowscommonDevices.get_device_status(device_serial=dut1.serial)
         if res != 'green':
-            pytest.fail('Status for serial {} not equal to Green: {}'.format(dut1.serial, res))
+            logger.fail('Status for serial {} not equal to Green: {}'.format(dut1.serial, res))
         else:
             logger.info('Status for device with serial number: {} is equal to Green'.format(dut1.serial))
 
@@ -171,7 +166,7 @@ class OnboardDeviceVSP4900UsingCsvTests:
         device_location = self.suite_udks.get_value_specific_column(xiq_library_at_class_level, dut1.serial, "LOCATION")
 
         if location_to_verify not in device_location:
-            pytest.fail('Current location {} did not match the expected location {} assigned to device {}'.format(
+            logger.fail('Current location {} did not match the expected location {} assigned to device {}'.format(
                 device_location, location_to_verify, dut1.serial))
         else:
             logger.info('Current location {} matched the expected location {} assigned to device {}'.format(
