9675c50a17edcc1fec3832613cf4550f06423bf7
shreenbanu <71691796+shreenbanu@users.noreply.github.com>
2023-04-17 11:15:35 -0400

    Exa 122 (#1405)
    * Update test_email_ids.py
    
    * update
    
    * Update ap_mu_private_pre_shared_key_XIQ-000.robot
    
    * Update test_email_ids.py
    
    * Update test_email_ids.py
    
    * Update test_email_ids.py
    
    * Update test_email_ids.py
    
    * update 2
    
    * update
    
    * Update topo.test.e1r1.xim.automation.yaml

--- Tests/Robot/Functional/XIQ/Wireless/Sanity/Resources/test_email_ids.py
+++ Tests/Robot/Functional/XIQ/Wireless/Sanity/Resources/test_email_ids.py
@@ -1,8 +1,14 @@
-MAIL_ID1 =			'blrtb2tenant1@gmail.com'
-MAIL_ID1_PASS =		'ozrnzboilbidupin'
+from robot.libraries.BuiltIn import BuiltIn
 
-MAIL_ID2 =			'blrtb2tenant2@gmail.com'
-MAIL_ID2_PASS =		'Extreme@123'
+MAIL_ID1 = BuiltIn().get_variable_value("${PPSK_MAIL_ID}")
+MAIL_ID1_PASS = BuiltIn().get_variable_value("${PPSK_MAIL_PASSWORD}")
 
-MAIL_ID3 =			'blrtb2tenant3@gmail.com'
-MAIL_ID3_PASS =		'Extreme@123'
+if MAIL_ID1 is None:
+    PPSK_MAIL_ID = 'blrtb2tenant1@gmail.com'
+    PPSK_MAIL_PASSWORD = 'ozrnzboilbidupin'
+else:
+    PPSK_MAIL_ID = MAIL_ID1
+    PPSK_MAIL_PASSWORD = MAIL_ID1_PASS
+
+MAIL_ID2 = 'blrtb2tenant2@gmail.com'
+MAIL_ID2_PASS = 'Extreme@123'
